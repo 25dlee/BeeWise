c921a4d87c0e7c38d121b99c0103b1fd94b30cc5
Kelly Xu <kelly.xu@duke.edu>
2025-02-09 00:04:23 -0500

    refactored file names and page routes to be more informative

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import SecondPage from "./SecondPage";
-import ThirdPage from "./ThirdPage";       // <-- new import
-import ProductNavbar from "./ProductNavbar";
+import MissionsPage from "./MissionsPage";
+import TopicDetailPage from "./TopicDetailPage";       // <-- new import
+import Navbar from "./Navbar";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <ProductNavbar />
+      <Navbar />
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/second" element={<SecondPage />} />
+          <Route path="/missions" element={<MissionsPage />} />
           {/* New third page route */}
-          <Route path="/third" element={<ThirdPage />} />
+          <Route path="/topicdetail" element={<TopicDetailPage />} />
         </Routes>
       </div>
     </Router>
