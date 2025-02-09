46ae6e1c440680319edbd1d66dc3f60f2a03ae3e
Kelly Xu <kelly.xu@duke.edu>
2025-02-08 19:07:08 -0500

    frontend skeleton set up

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,9 @@ from .routes import main  # Importing the routes
 
 app = Flask(__name__)
 
+from flask_cors import CORS
+CORS(app, origins=["http://localhost:3000"]) #only allow React on localhost:3000
+
 # Register the Blueprint 
 app.register_blueprint(main)  
 
