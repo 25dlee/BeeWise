a6f11bae5f3236168e89f00fbc2b9d6bd9bb3ec6
Pierce Brookins <43153860+retrocomputernerd@users.noreply.github.com>
2025-02-09 01:39:11 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import MissionsPage from "./MissionsPage";
-import TopicDetailPage from "./TopicDetailPage";
-import UserProfilePage from "./UserProfilePage";
-import HomePage from "./HomePage";
-import SocialPage from "./SocialPage";
+import TopicDetailPage from "./TopicDetailPage";  
 import Navbar from "./Navbar";
 import "./App.css";
+import CNNNews1 from "./CNNNews1";
+import FoxNews1 from "./FoxNews1";
+import APNews1 from "./APNews1";
+import News from "./News";
+
 
 function App() {
   return (
@@ -16,12 +18,14 @@ function App() {
       <Navbar />
       <div>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Home />} />
           <Route path="/missions" element={<MissionsPage />} />
-          {/* TODO: make username a dynamic parameter in the path*/}
-          <Route path="/social" element={<SocialPage />} />
-          {/* TODO: make username a dynamic parameter in the path*/}
-          <Route path="/profile" element={<UserProfilePage />} />
+          <Route path="/news1-cnn" element={<CNNNews1 />} />
+          <Route path="/news1-fox" element={<FoxNews1 />} />
+          <Route path="/news1-ap" element={<APNews1 />} />
+          <Route path="/news" element={<News />} />
+          {/* New third page route */}
+          <Route path="/topicdetail" element={<TopicDetailPage />} />
         </Routes>
       </div>
     </Router>
